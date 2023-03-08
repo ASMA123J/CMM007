0408d3cfa0a87011c1bdb9cba5513a21e2d5f8f0
ASMA123J <124143121+ASMA123J@users.noreply.github.com>
2023-03-08 11:55:42 +0000

    Add files via upload

--- LAB9/PHPStorm/connection.php
+++ LAB9/PHPStorm/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$dbname='smdb';
-$username = "root";
-$password = "root";
+$dbname='db2201532_cmm007';
+$username = "2201532";
+$password = "2201532";
 // Create connection
 $db = new mysqli($servername, $username, $password, $dbname);
 // Check connection
